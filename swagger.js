6fd0f87e661a991698efd75043cb5085d34064de
Seth Harrington <sbgha.09@gmail.com>
2026-01-20 14:08:55 -0600

    correcting swagger URL prefix

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: "My API",
     description: "Description",
   },
-  host: "https://get-contacts-api.onrender.com",
+  host: "get-contacts-api.onrender.com",
   // host: "localhost:8080", // Use localhost for local development
   schemes: ["http", "https"], // Use both https and http for production
 };
