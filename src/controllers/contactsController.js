8daf8499b47ab92ae85f4b7574b167d9423bd53a
Seth Harrington <sbgha.09@gmail.com>
2026-01-20 13:58:45 -0600

    Adding post, put, delete

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -32,7 +32,56 @@ const getById = async (req, res) => {
     });
 };
 
+const createContact = async (req, res) => {
+  console.log("Creating contact with data:", req.body);
+  const newContact = req.body;
+  const result = await mongodb
+    .getDatabase()
+    .collection("Contacts")
+    .insertOne(newContact);
+  if (result.acknowledged) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(201).json(result.insertedId);
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).json({ error: "Failed to create contact" });
+  }
+};
+
+const updateContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const updatedContact = req.body;
+  const result = await mongodb
+    .getDatabase()
+    .collection("Contacts")
+    .updateOne({ _id: contactId }, { $set: updatedContact });
+  if (result.modifiedCount > 0) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ message: "Contact updated successfully" });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).json({ error: "Failed to update contact" });
+  }
+};
+const deleteContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDatabase()
+    .collection("Contacts")
+    .deleteOne({ _id: contactId });
+  if (result.deletedCount > 0) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ message: "Contact deleted successfully" });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).json({ error: "Failed to delete contact" });
+  }
+};
+
 module.exports = {
   getAll,
   getById,
+  createContact,
+  updateContact,
+  deleteContact,
 };
--- src/routes/contactsRouter.js
+++ src/routes/contactsRouter.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 const contactsController = require("../controllers/contactsController");
 router.get("/", contactsController.getAll);
 router.get("/:id", contactsController.getById);
-
+router.post("/", contactsController.createContact);
+router.put("/:id", contactsController.updateContact);
+router.delete("/:id", contactsController.deleteContact);
 module.exports = router;
